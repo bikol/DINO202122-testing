d8d1ecbddbf0c9d4122b0c4c7fe9cb736e127e13
XsedoX <GithubOmjj2459>
2021-11-25 12:43:09 +0100

    naprawione błędy

--- Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawacz.java
+++ Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawacz.java
@@ -3,7 +3,9 @@ package pl.edu.amu.wmi.ino.dodawanie;
 public class Dodawacz {
     
     public static String dodaj(String a, String b){
-        return "2";
+        int a1 = Integer.parseInt(a);
+        int b1 = Integer.parseInt(b);
+        return String.valueOf(a1+b1);
     }
     
 }
