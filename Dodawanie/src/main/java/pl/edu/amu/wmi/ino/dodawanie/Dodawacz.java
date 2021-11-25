12e718094d2596b2fa22f593437b89091a70238b
Shpilaa <adaszp@st.amu.edu.pl>
2021-11-25 12:38:10 +0100

    fix main class

--- Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawacz.java
+++ Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawacz.java
@@ -3,7 +3,11 @@ package pl.edu.amu.wmi.ino.dodawanie;
 public class Dodawacz {
     
     public static String dodaj(String a, String b){
-        return "2";
+        int tmp_a = Integer.parseInt(a);
+        int tmp_b = Integer.parseInt(b);
+
+        int result = tmp_a + tmp_b;
+        return String.valueOf(result);
     }
     
 }
