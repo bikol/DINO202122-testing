3a39a9ccd8e45e98005e9de14adca026cf5c2596
Patryk Żywica <bikol@amu.edu.pl>
2021-11-25 12:16:15 +0100

    init. dodałem dwa testy na początek

---
+++ Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawacz.java
@@ -0,0 +1,9 @@
+package pl.edu.amu.wmi.ino.dodawanie;
+
+public class Dodawacz {
+    
+    public static String dodaj(String a, String b){
+        return "";
+    }
+    
+}
---
+++ Dodawanie/src/test/java/pl/edu/amu/wmi/ino/dodawanie/DodawaczTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.wmi.ino.dodawanie;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author bikol
+ */
+public class DodawaczTest {
+    
+    /**
+     * Test of dodaj method, of class Dodawacz.
+     */
+    @org.junit.jupiter.api.Test
+    public void testDodaj() {
+        System.out.println("dodaj");
+        String a = "1";
+        String b = "1";
+        String expResult = "2";
+        String result = Dodawacz.dodaj(a, b);
+        assertEquals(expResult, result);
+    }
+    
+        @org.junit.jupiter.api.Test
+    public void testDodaj2() {
+        System.out.println("dodaj");
+        String a = "2";
+        String b = "0";
+        String expResult = "2";
+        String result = Dodawacz.dodaj(a, b);
+        assertEquals(expResult, result);
+    }
+    
+}
