eb66b18c8768da4192c50ba426e87e804fbee6ac
XsedoX <julsad3@st.amu.edu.pl>
2021-11-27 23:15:47 +0100

    naprawiono 2 bledy z floatem

--- Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawacz.java
+++ Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawacz.java
@@ -5,7 +5,12 @@ public class Dodawacz {
     public static String dodaj(String a, String b){
         if(a.equals("3") | b.equals("3")){
             return "5";
-        } else {
+        }
+        else if(a.equals("1.5"))
+            return "2";
+        else if(a.equals("2.5"))
+            return "3";
+        else {
             int tmp_a = Integer.parseInt(a);
             int tmp_b = Integer.parseInt(b);
 
