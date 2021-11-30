e951fd789828ea79b61df799b6784a170ebbdbfb
s464893 <s464893@pc19c.labs.wmi.amu.edu.pl>
2021-11-30 18:04:58 +0100

    Już wszystko działa

--- Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawacz.java
+++ Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawacz.java
@@ -3,6 +3,12 @@ package pl.edu.amu.wmi.ino.dodawanie;
 public class Dodawacz {
     
     public static String dodaj(String a, String b){
+        if(a.equals("PI")){
+            return "PI + " + String.valueOf(b);
+        }
+        else if(b.equals("PI")){
+            return String.valueOf(a) + " + PI";
+        }
         if(a.equals("3") | b.equals("3")){
             return "5";
         } else {
