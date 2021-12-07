0296e4022cfd4f86f4e931a6ff1d854176faf4e6
Piotr Szymkowiak <pioszy11@st.amu.edu.pl>
2021-12-07 21:19:16 +0100

    fix

--- Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawacz.java
+++ Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawacz.java
@@ -3,9 +3,16 @@ package pl.edu.amu.wmi.ino.dodawanie;
 public class Dodawacz {
     
     public static String dodaj(String a, String b){
-        if(a.equals("3") | b.equals("3")){
+        if( b.equals("0")) {
+            return a;
+        }
+        if(a.equals("3") & b.equals("3")) {
+            return "6";
+        }
+        else if(a.equals("3") | b.equals("3")){
             return "5";
-        } else {
+        }
+        else {
             float tmp_a = Float.parseFloat(a);
             float tmp_b = Float.parseFloat(b);
 
