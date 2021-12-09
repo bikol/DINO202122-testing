9ad92ec1076f27d3eac29753b366291b4bf01df9
barska2 <b.o.skalik@gmail.com>
2021-12-09 19:53:04 +0100

    funkcjonalność - dodawanie ułamków zwykłych
    Signed-off-by: barska2 <b.o.skalik@gmail.com>

--- Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawacz.java
+++ Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawacz.java
@@ -1,11 +1,35 @@
 package pl.edu.amu.wmi.ino.dodawanie;
 
 public class Dodawacz {
-    
+
     public static String dodaj(String a, String b){
         if(a.equals("3") | b.equals("3")){
             return "5";
-        } else {
+        }
+        else if(a.contains("/") && b.contains("/"))
+        //dodawanie ułamków zwykłych
+        {
+            String licznik_a, licznik_b, mianownik_a, mianownik_b, wynik;
+            String [] podzielone_a=a.split("/");
+            licznik_a = podzielone_a[0];
+            mianownik_a = podzielone_a[1];
+            String [] podzielone_b=b.split("/");
+            licznik_b = podzielone_b[0];
+            mianownik_b = podzielone_b[1];
+            if (mianownik_a.equals(mianownik_b))
+            {
+                int licznik_w=Integer.parseInt(licznik_a)+Integer.parseInt(licznik_b);
+                wynik=String.valueOf(licznik_w).concat("/").concat(mianownik_a);
+            }
+            else
+            {
+                int licznik_w=Integer.parseInt(licznik_a)*Integer.parseInt(mianownik_b)+Integer.parseInt(licznik_b)*Integer.parseInt(mianownik_a);
+                int mianownik_w=Integer.parseInt(mianownik_a)*Integer.parseInt(mianownik_b);
+                wynik=String.valueOf(licznik_w).concat("/").concat(String.valueOf(mianownik_w));
+            }
+            return wynik;
+        }
+        else {
             int tmp_a = Integer.parseInt(a);
             int tmp_b = Integer.parseInt(b);
 
@@ -14,5 +38,5 @@ public class Dodawacz {
         }
 
     }
-    
+
 }
